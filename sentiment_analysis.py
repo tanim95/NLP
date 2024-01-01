905f9862cddd765efbdbf05cc4707a474a803512
tanim95 <tanimrahman78@gmail.com>
2024-01-01 21:46:55 +0600

    nlp with keras ,lstm to predict nest words

--- sentiment_analysis.py
+++
@@ -1,8 +0,0 @@
-import spacy
-
-nlp = spacy.load('en_core_web_lg')
-# print(nlp(u'tiger').vector)
-tokens = nlp(u'lion cat pet')
-for i in tokens:
-  for j in tokens:
-    print(i.text,j.text,i.similarity(j))
